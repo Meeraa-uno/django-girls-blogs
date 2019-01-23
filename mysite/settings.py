d012ef5fedaac741cbc917f4808c7892fd8a543e
Meeraa_uno <njayachandran@unomaha.edu>
2019-01-23 16:20:29 -0600

    second push

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -54,7 +53,6 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
